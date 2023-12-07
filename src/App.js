71371a4808861c99d10bf151e97b6d8bb317d9bc
Srinithi <balasubramaniansrinithi@gmail.com>
2023-12-07 12:37:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Name:Srinithi B</h4>
+      <h4>Department:Computer Science and Engineering</h4>
+      <h4>Year:3rd-year</h4>
+      <h4>Register Number:73772114210</h4>
+      <h4>Area of interest:Android development</h4>
+    </div>
+  );
+}
+
+export default App;
